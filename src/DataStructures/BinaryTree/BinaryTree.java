5fa7fa6feedc24ddd442528c909a49253f04a67e
Kanhaiya Lal <kanhaiya.lal2@delhivery.com>
2021-03-29 08:28:25 +0530

    add construction of binary tree from it's inorder traversal and postorder traversal.

--- src/DataStructures/BinaryTree/BinaryTree.java
+++ src/DataStructures/BinaryTree/BinaryTree.java
@@ -23,6 +23,32 @@ public class BinaryTree{
 		}
 	}
 	
+	public static Node<String> constructFromPostOrder(List<String> inorder, List<String> postorder){
+		Map<String, Integer> inorderIndex = new HashMap<>();
+		for(int i=0; i<inorder.size(); i++){
+			inorderIndex.put(inorder.get(i), i);
+		}
+		return constructFromPostOrderHelper(0, inorder.size()-1, 0, postorder.size()-1, postorder, inorderIndex);
+	}
+	
+	public static Node<String> constructFromPostOrderHelper(int inorder_start, int inorder_end, int postorder_start, int postorder_end, List<String> postorder, Map<String, Integer>inorderIndex){
+		if( inorder_start > inorder_end || postorder_start> postorder_end)
+			return null;
+		int rootIndex = inorderIndex.get(postorder.get(postorder_end));
+		int leftSubtreeSize = rootIndex - inorder_start;
+		Node<String> root = new Node(postorder.get(postorder_end));
+		/**
+		System.out.println("root data->"+ root.data);
+		System.out.println(String.format("inorder_start:%s, inorder_end:%s, postorder_start:%s, postorder_end:%s, size:%s", inorder_start,
+		inorder_end, postorder_start, postorder_end, postorder.size()));
+		System.out.println(String.format("postorder left: %s", postorder.subList(postorder_start, postorder_start + leftSubtreeSize)));
+		System.out.println(String.format("postorder right: %s", postorder.subList(postorder_start + leftSubtreeSize, postorder_end-1))); 
+		*/
+		root.left = constructFromPostOrderHelper(inorder_start, rootIndex-1, postorder_start, postorder_start+leftSubtreeSize-1, postorder, inorderIndex);
+		root.right = constructFromPostOrderHelper(rootIndex+1, inorder_end, postorder_start+leftSubtreeSize, postorder_end-1, postorder, inorderIndex);
+		return root;
+	}
+	
 	public static <T> boolean isSymmetricHelper(Node<T> left, Node<T> right){
 		
 		if (left == null && right != null)
@@ -44,19 +70,10 @@ public class BinaryTree{
 	}
 
 	public static void main(String args[]){
-		
-		Node<Integer> A = new Node<>(314);
-		Node<Integer> B =  A.left = new Node<>(6);
-		Node<Integer> E = A.right = new Node<>(6);
-		Node<Integer> C = B.right = new Node<>(2);
-		Node<Integer> F = E.left = new Node<>(2);
-		Node<Integer> D = C.right = new Node<>(3);
-		Node<Integer> G = F.left = new Node<>(4);
-		boolean ans = isSymmetric(A);
-		if (ans)
-			System.out.println("Tree is Symmetrical.");
-		else
-			System.out.println("Tree is Asymmetrical.");
+		List<String> inorder = new ArrayList<>(List.of("F", "B", "A", "E","H", "C", "D", "I", "G"));
+		List<String> postorder = new ArrayList<>(List.of("F", "A", "E", "B", "I", "G", "D", "C", "H"));
+		Node<String> root = constructFromPostOrder(inorder, postorder);
+		inorder(root);
 		
 	}
 
